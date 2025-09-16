72c91d8c97146f771fdb2d5e2c2a6ce3583651a8
Sahith Akula <akulasahith268@gmail.com>
2025-09-16 16:20:15 +0530

    Guide schema updated

--- src/models/Guide.js
+++ src/models/Guide.js
@@ -1,8 +1,11 @@
+import mongoose from "mongoose";
+
 const guideSchema = new mongoose.Schema(
   {
     vendorId: { type: mongoose.Schema.Types.ObjectId, ref: "Vendor", required: true },
     costPerDay: { type: Number, required: true },
     availability: { type: Boolean, default: true },
+    location: { type: String, required: true }, // Plain text location
   },
   { timestamps: true }
 );
