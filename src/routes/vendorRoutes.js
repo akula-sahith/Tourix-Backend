9ef4a5995178bbb3b17835a89da28caf2898a3b5
Sahith Akula <akulasahith268@gmail.com>
2025-09-16 15:11:32 +0530

    Routes updated

--- src/routes/vendorRoutes.js
+++ src/routes/vendorRoutes.js
@@ -1,12 +1,12 @@
 // routes/vendorRoutes.js
-import express from "express";
-import {
+const express = require("express");
+const {
   registerVendor,
   loginVendor,
   getAllVendors,
   getVendorById,
   verifyVendor,
-} from "../controllers/vendorController.js";
+} = require("../controllers/vendorController");
 
 const router = express.Router();
 
@@ -15,10 +15,8 @@ router.post("/register", registerVendor);  // Register vendor
 router.post("/login", loginVendor);        // Login vendor
 
 // Protected/Admin routes (you can later add auth middleware here)
-router.get("/", getAllVendors);            // Get all vendors
+router.get("/getAll", getAllVendors);            // Get all vendors
 router.get("/:id", getVendorById);         // Get vendor by ID
 router.patch("/:id/verify", verifyVendor); // Verify vendor (admin use)
 
-export default router;
-
-Give module.exports
+module.exports = router;
